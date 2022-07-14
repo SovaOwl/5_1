c070957e240cccb4b60e3862fbc2b94717d141e5
SovaOwl <sova27755@gmail.com>
2022-07-14 16:33:03 +0300

    First commit

---
+++ 5_1/5_1.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+ struct Figure
+ {
+ private:
+	 int sides_figure = 0;
+
+ public:
+	 virtual string display_name()
+	 {
+		 return "Фигура";
+	 }
+	 virtual int display_sides_figure()
+	 {
+		 return sides_figure;
+	 }
+
+ };
+
+ struct Triangle: public Figure
+ {
+ private:
+	 int sides_figure = 3;
+
+ public:
+	 string display_name() override
+	 {
+		 return "Треугольник";
+	 }
+	 int display_sides_figure() override
+	 {
+		 return sides_figure;
+	 }
+ 
+ }; 
+ 
+ struct Quadrangle: public Figure
+ {
+ private:
+	 int sides_figure = 4;
+
+ public:
+	 string display_name() override
+	 {
+		 return "Четырёхугольник";
+	 }
+	 int display_sides_figure() override
+	 {
+		 return sides_figure;
+	 }
+ 
+ };
+
+ void print_figures(Figure* figure)
+ {
+	 cout << figure->display_name() << ": " << figure->display_sides_figure() << endl;
+ }
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	Figure figure;
+	Triangle triangle;
+	Quadrangle quadrangle;
+
+	print_figures(&figure);
+	print_figures(&triangle);
+	print_figures(&quadrangle);
+
+}
